02817961d695981ed5ddc71428529ce807a93c45
Michal Muszynski <m.muszy@gmail.com>
2018-11-07 18:29:41 +0100

    Add element with proper id

--- assets/main.js
+++ assets/main.js
@@ -2,20 +2,23 @@ const addPoint = document.querySelector('#add-point');
 
 addPoint.addEventListener('click', createInputs)
 
+let counter = 2;
 function createInputs() {
   const newInput = 
-  `
-<div class="coordinates-set">
-  <label>
-    X
-    <input type="text" class="coordinate-x">
-  </label>
-  <label>
-    Y
-    <input type="text" class="coordinate-y">
-  </label>
-</div>
-  `
+    `
+    <p>${counter}.</p>
+    <div class="coordinates-set" id=${counter}>
+      <label>
+        X
+        <input type="text" class="coordinate-x">
+      </label>
+      <label>
+        Y
+        <input type="text" class="coordinate-y">
+      </label>
+    </div>
+    `
   const inputContainer = document.querySelector('.coordinates-values');
   inputContainer.insertAdjacentHTML('beforeend', newInput);
+  counter += 1;
 }
