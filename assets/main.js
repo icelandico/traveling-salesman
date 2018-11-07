e4320279ed20692d3f73f5aba9eaf7e449e1b864
Michal Muszynski <m.muszy@gmail.com>
2018-11-07 18:14:36 +0100

    Adding inputs onclick

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,21 @@
+const addPoint = document.querySelector('#add-point');
 
+addPoint.addEventListener('click', createInputs)
+
+function createInputs() {
+  const newInput = 
+  `
+<div class="coordinates-set">
+  <label>
+    X
+    <input type="text" class="coordinate-x">
+  </label>
+  <label>
+    Y
+    <input type="text" class="coordinate-y">
+  </label>
+</div>
+  `
+  const inputContainer = document.querySelector('.coordinates-values');
+  inputContainer.insertAdjacentHTML('beforeend', newInput);
+}
