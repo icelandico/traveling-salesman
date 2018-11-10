566aa1df4ea429c8b7d7e522eac81a00f25ab866
Michal Muszynski <m.muszy@gmail.com>
2018-11-10 18:21:24 +0100

    Calculate segments distances

--- assets/main.js
+++ assets/main.js
@@ -33,13 +33,8 @@ function calculateDistances() {
   console.log(coordinates)
   console.log(paths)
   console.log(segments)
-  /* Calculate distances using Math.hypot() */
-  // const distances = paths.map(path => path.reduce(function(a,b) {
-  //   return Math.hypot(coordinates[paths[b][b]].x - coordinates[paths[a][a].x], coordinates[paths[a][b]].y - coordinates[paths[a][a]].y)
-  // }))
-  // const pathsSet = [ [0,1], [1,2], [2,3] ];
-  // const pointsSquare = pathsSet.map(item => ([ coordinates[item[1]].x - coordinates[item[0]].x, coordinates[item[1]].y - coordinates[item[0]].y ]));
-  // console.log(pointsSquare);
+  const segmentDistances = segments.map(segment => segment.map(pair => Math.hypot(coordinates[pair[1]].x - coordinates[pair[0]].x, coordinates[pair[1]].y - coordinates[pair[0]].y)))
+  console.log(segmentDistances)
 }
 
 function middleSegments(segments) {
