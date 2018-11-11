8bb42772d75a1d1eb885486608e350362aa38c9c
Michal Muszynski <m.muszy@gmail.com>
2018-11-11 15:31:00 +0100

    Add Jest as testing module

--- assets/tests/getCoordinates.test.js
+++ assets/tests/getCoordinates.test.js
@@ -1,32 +1,31 @@
 const getCoordinates = require('./getCoordinates.js');
 
-if (getCoordinates(
-  [
-    {id: 0, x: 2, y: 1},
-    {id: 1, x: NaN, y: 10},
-    {id: 2, x: 5, y: 2}
-  ]
-) !== 1) {
-  throw new Error('Check fail Example 1')
-}
+it('Coordinates', () => {
+  expect(getCoordinates(
+    [
+      {id: 0, x: 2, y: 1},
+      {id: 1, x: NaN, y: 10},
+      {id: 2, x: 5, y: 2}
+    ]
+  )).toBe(1)
+})
 
-if (getCoordinates(
-  [
-    {id: 0, x: 2, y: 1},
-    {id: 1, x: NaN, y: 10},
-    {id: 2, x: NaN, y: 2}
-  ]
-) !== 2) {
-  throw new Error('Check fail Example 2')
-}
+it('Coordinates', () => {
+  expect(getCoordinates(
+    [
+      {id: 0, x: 2, y: 1},
+      {id: 1, x: NaN, y: 10},
+      {id: 2, x: NaN, y: 2}
+    ]
+  )).toBe(2)
+})
 
-if (getCoordinates(
-  [
-    {id: 0, x: 2, y: NaN},
-    {id: 1, x: NaN, y: 10},
-    {id: 2, x: NaN, y: 2}
-  ]
-) !== 3) {
-  throw new Error('Check fail Example 3')
-}
-
+it('Coordinates', () => {
+  expect(getCoordinates(
+    [
+      {id: 0, x: 2, y: NaN},
+      {id: 1, x: NaN, y: 10},
+      {id: 2, x: NaN, y: 2}
+    ]
+  )).toBe(3)
+})
