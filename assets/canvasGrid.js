8ede8259f1f6569e662c691712dbf8337a69d4fb
Michal Muszynski <m.muszy@gmail.com>
2021-01-16 19:57:28 +0100

    Format code

--- assets/canvasGrid.js
+++ assets/canvasGrid.js
@@ -12,56 +12,55 @@ const drawGrid = () => {
   const x_axis_starting_point = 1;
   const y_axis_starting_point = 1;
 
-// Draw grid lines along X-axis
-  for(let i = 0; i <= num_lines_x; i++) {
+  // Draw grid lines along X-axis
+  for (let i = 0; i <= num_lines_x; i++) {
     ctx.beginPath();
     ctx.lineWidth = 1;
 
     // If line represents X-axis draw in different color
-    if(i == x_axis_distance_grid_lines)
-      ctx.strokeStyle = "#000000";
-    else
-      ctx.strokeStyle = "#acacac";
+    if (i === x_axis_distance_grid_lines) ctx.strokeStyle = "#000000";
+    else ctx.strokeStyle = "#acacac";
 
-    if (i == num_lines_x) {
+    if (i === num_lines_x) {
       ctx.moveTo(0, grid_size * i);
       ctx.lineTo(canvas_width, grid_size * i);
-    }
-    else {
+    } else {
       ctx.moveTo(0, grid_size * i + 0.5);
       ctx.lineTo(canvas_width, grid_size * i + 0.5);
     }
     ctx.stroke();
   }
 
-// Draw grid lines along Y-axis
-  for(let i = 0; i <= num_lines_y; i++) {
+  // Draw grid lines along Y-axis
+  for (let i = 0; i <= num_lines_y; i++) {
     ctx.beginPath();
     ctx.lineWidth = 1;
 
     // If line represents X-axis draw in different color
-    if (i == y_axis_distance_grid_lines) {
+    if (i === y_axis_distance_grid_lines) {
       ctx.strokeStyle = "#121418";
     } else {
-        ctx.strokeStyle = "#acacac";
-      }
+      ctx.strokeStyle = "#acacac";
+    }
 
-    if(i == num_lines_y) {
+    if (i === num_lines_y) {
       ctx.moveTo(grid_size * i, 0);
       ctx.lineTo(grid_size * i, canvas_height);
-    }
-    else {
+    } else {
       ctx.moveTo(grid_size * i + 0.5, 0);
       ctx.lineTo(grid_size * i + 0.5, canvas_height);
     }
     ctx.stroke();
   }
 
-// Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
-  ctx.translate(y_axis_distance_grid_lines * grid_size, x_axis_distance_grid_lines * grid_size);
+  // Translate to the new origin. Now Y-axis of the canvas is opposite to the Y-axis of the graph. So the y-coordinate of each element will be negative of the actual
+  ctx.translate(
+    y_axis_distance_grid_lines * grid_size,
+    x_axis_distance_grid_lines * grid_size
+  );
 
-// Ticks marks along the positive X-axis
-  for(let i = 1; i <(num_lines_y - y_axis_distance_grid_lines); i++) {
+  // Ticks marks along the positive X-axis
+  for (let i = 1; i < num_lines_y - y_axis_distance_grid_lines; i++) {
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#000000";
@@ -72,13 +71,13 @@ const drawGrid = () => {
     ctx.stroke();
 
     // Text value at that point
-    ctx.font = '11px Consolas';
-    ctx.textAlign = 'start';
+    ctx.font = "11px Consolas";
+    ctx.textAlign = "start";
     ctx.fillText(`${x_axis_starting_point * i}`, grid_size * i - 2, 15);
   }
 
-// Ticks marks along the negative X-axis
-  for(let i = 1; i < y_axis_distance_grid_lines; i++) {
+  // Ticks marks along the negative X-axis
+  for (let i = 1; i < y_axis_distance_grid_lines; i++) {
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#000000";
@@ -89,14 +88,14 @@ const drawGrid = () => {
     ctx.stroke();
 
     // Text value at that point
-    ctx.font = '11px Consolas';
-    ctx.textAlign = 'end';
+    ctx.font = "11px Consolas";
+    ctx.textAlign = "end";
     ctx.fillText(`${-x_axis_starting_point * i}`, -grid_size * i + 3, 15);
   }
 
-// Ticks marks along the positive Y-axis
-// Positive Y-axis of graph is negative Y-axis of the canvas
-  for(let i = 1; i < (num_lines_x - x_axis_distance_grid_lines); i++) {
+  // Ticks marks along the positive Y-axis
+  // Positive Y-axis of graph is negative Y-axis of the canvas
+  for (let i = 1; i < num_lines_x - x_axis_distance_grid_lines; i++) {
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#000000";
@@ -107,14 +106,14 @@ const drawGrid = () => {
     ctx.stroke();
 
     // Text value at that point
-    ctx.font = '11px Consolas';
-    ctx.textAlign = 'start';
+    ctx.font = "11px Consolas";
+    ctx.textAlign = "start";
     ctx.fillText(`${-y_axis_starting_point * i}`, 8, grid_size * i + 3);
   }
 
-// Ticks marks along the negative Y-axis
-// Negative Y-axis of graph is positive Y-axis of the canvas
-  for(let i = 1; i < x_axis_distance_grid_lines; i++) {
+  // Ticks marks along the negative Y-axis
+  // Negative Y-axis of graph is positive Y-axis of the canvas
+  for (let i = 1; i < x_axis_distance_grid_lines; i++) {
     ctx.beginPath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = "#000000";
@@ -125,35 +124,49 @@ const drawGrid = () => {
     ctx.stroke();
 
     // Text value at that point
-    ctx.font = '11px Consolas';
-    ctx.textAlign = 'start';
+    ctx.font = "11px Consolas";
+    ctx.textAlign = "start";
     ctx.fillText(`${y_axis_starting_point * i}`, 8, -grid_size * i + 3);
   }
-
 };
 
 const canvasPoints = document.getElementById("chart-points");
 const canvasPointsContext = canvasPoints.getContext("2d");
-canvasPointsContext.translate(y_axis_distance_grid_lines * grid_size, x_axis_distance_grid_lines * grid_size);
+canvasPointsContext.translate(
+  y_axis_distance_grid_lines * grid_size,
+  x_axis_distance_grid_lines * grid_size
+);
 
 function drawPoint(x, y) {
-  canvasPointsContext.moveTo(grid_size * x + 0.5, grid_size * y + 0.5)
-  canvasPointsContext.arc(grid_size * x + 0.5, grid_size * y * (-1) + 0.5, 6, 0, Math.PI * 2, true);
+  canvasPointsContext.moveTo(grid_size * x + 0.5, grid_size * y + 0.5);
+  canvasPointsContext.arc(
+    grid_size * x + 0.5,
+    grid_size * y * -1 + 0.5,
+    6,
+    0,
+    Math.PI * 2,
+    true
+  );
 }
 
 function clearCanvasContext() {
   canvasPointsContext.restore();
-  canvasPointsContext.clearRect( -x_axis_distance_grid_lines * grid_size, -y_axis_distance_grid_lines * grid_size, canvas_width, canvas_height);
+  canvasPointsContext.clearRect(
+    -x_axis_distance_grid_lines * grid_size,
+    -y_axis_distance_grid_lines * grid_size,
+    canvas_width,
+    canvas_height
+  );
 }
 
 function drawPointsLayer(points) {
-  clearCanvasContext()
-  points.forEach(point => {
+  clearCanvasContext();
+  points.forEach((point) => {
     canvasPointsContext.beginPath();
-    drawPoint(point.x, point.y)
+    drawPoint(point.x, point.y);
     canvasPointsContext.fillStyle = "#1e3383";
     canvasPointsContext.fill();
-  })
+  });
 }
 
 drawGrid();
